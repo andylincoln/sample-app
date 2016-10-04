6b51c7b219f1dbea08f2d068bf9e457a8f975d78
Andy Lincoln <andrewlincoln11@gmail.com>
2016-10-04 11:30:43 -0400

    8.2.5 Login upon signup - also has hotfix for is_logged_in? not working in test env

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,5 +1,14 @@
 require 'test_helper'
 
+# TODO Figure out why this not being here throws an error.
+# Seems to be particular to this method, because a basic hello world
+# method in test_helper doesn't seem to be affected by this
+# changing the name doesn't work either
+
+def is_logged_in?
+    !session[:user_id].nil?
+end
+
 class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
@@ -12,14 +21,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       assert_template 'users/new'
     end
 
-    test "valid signup information" do
-      get signup_path
-      assert_difference 'User.count', 1 do
-        post_via_redirect users_path, user: { name:  "Example User",
-                                              email: "user@example.com",
-                                              password:              "password",
-                                              password_confirmation: "password" }
-      end
-      assert_template 'users/show'
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name:  "Example User",
+                                            email: "user@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password" }
     end
+    assert_template 'users/show'
+    assert is_logged_in?
+  end
 end
