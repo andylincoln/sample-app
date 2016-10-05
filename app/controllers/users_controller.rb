1adb0d2bba2d5af3c7f4acbfc966d98e41bd925d
Andy Lincoln <andrewlincoln11@gmail.com>
2016-10-04 20:38:46 -0400

    Listing 9.42: Paginating the users in the index action.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,19 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
-
+  
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
-
+  
   def show
     @user = User.find(params[:id])
   end
-
+  
   def new
     @user = User.new
   end
-
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -24,11 +24,11 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
+  
   def edit
     @user = User.find(params[:id])
   end
-
+  
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
@@ -38,27 +38,27 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-
+  
   private
-
+  
   def user_params
     params.require(:user).permit(:name, :email, :password,
     :password_confirmation)
   end
-
+  
   # Confirms a logged-in user.
   def logged_in_user
     unless logged_in?
       store_location
       flash[:danger] = "Please log in."
-        redirect_to login_url
+      redirect_to login_url
     end
   end
-
+  
   # Confirms the correct user.
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
   end
-
+  
 end
